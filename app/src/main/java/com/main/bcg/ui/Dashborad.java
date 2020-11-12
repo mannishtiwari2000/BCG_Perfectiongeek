0e195e091ddfa696949bf0951cfe9f81227343b6
Manish Tiwari <61265561+mannishtiwari2000@users.noreply.github.com>
2020-11-12 11:21:50 +0530

    Recycler view

--- app/src/main/java/com/main/bcg/ui/Dashborad.java
+++ app/src/main/java/com/main/bcg/ui/Dashborad.java
@@ -1,5 +1,6 @@
 package com.main.bcg.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,7 +9,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.main.bcg.R;
+
+import java.util.ArrayList;
 
 public class Dashborad extends Fragment {
 
@@ -17,7 +25,27 @@ public class Dashborad extends Fragment {
             ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.dashboard, container, false);
+        PieChart pieChart = root.findViewById(R.id.piechart);
 
+
+        ArrayList<PieEntry> entries =new ArrayList<>();
+        entries.add(new PieEntry(1000,"2017"));
+        entries.add(new PieEntry(900,"2018"));
+        entries.add(new PieEntry(800,"2019"));
+        entries.add(new PieEntry(700,"2020"));
+
+
+        PieDataSet pieDataSet=new PieDataSet(entries,"visitor");
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(10f);
+
+        PieData pieData=new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setCenterText("Pie Chat");
+        pieChart.animate();
+        pieChart.animateXY(5000, 5000);
         return root;
     }
 }
